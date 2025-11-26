266047c3fffdacc9291c47617206f998958e718f
ujnss <unlinunlin@163.com>
2025-11-26 09:27:07 +0000

    chore: output vk(hex-string) and vk hash(bytes32)

--- script/src/bin/vkey.rs
+++ script/src/bin/vkey.rs
@@ -6,5 +6,8 @@ pub const ZKTLS_ELF: &[u8] = include_elf!("zktls-program");
 fn main() {
     let prover = ProverClient::builder().cpu().build();
     let (_, vk) = prover.setup(ZKTLS_ELF);
-    println!("{}", vk.bytes32());
+    println!("vk_hash {}", vk.bytes32());
+
+    let vk_bytes = bincode::serialize(&vk).expect("failed to serialize vk");
+    println!("vk_bytes {}", format!("0x{}", hex::encode(vk_bytes)));
 }
